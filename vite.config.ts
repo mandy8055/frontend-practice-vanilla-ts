51b415e0028fe108fb45e40c3844125bcb928ec5
Manuj Sankrit <msankrit8055@gmail.com>
2025-09-21 06:22:02 +0530

    arch: 🏗️ add vite config improvements for build

--- vite.config.ts
+++ vite.config.ts
@@ -8,5 +8,14 @@ export default defineConfig({
   },
   build: {
     outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        // Manually add each component - update this as you add more
+        'components/01-cta-button/index': 'components/01-cta-button/index.html',
+        'components/02-loader-spinner/index':
+          'components/02-loader-spinner/index.html',
+      },
+    },
   },
 });
